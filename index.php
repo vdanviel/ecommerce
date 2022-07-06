d04606e0da0ee7ec9fc3fd4926969c146d399843
Victor Daniel <victordn.araujo@gmail.com>
2022-07-05 23:09:14 -0300

    PHP Ecommerce. (Update 4)

--- index.php
+++ index.php
@@ -11,53 +11,60 @@ $app = new Slim();
 #CLIENT ROTES
 $app->get('/', function() {
     
-	require_once("vendor/PERSONAL/template/header-footer/header.php");
+	require_once("vendor/PERSONAL/template/client-site/header-footer/header.php");
 	require_once("vendor/PERSONAL/template/client-site/index.php");
-	require_once("vendor/PERSONAL/template/header-footer/footer.php");
+	require_once("vendor/PERSONAL/template/client-site/header-footer/footer.php");
 
 });
 
 $app->get('/lista-produtos', function() {
     
-	require_once("vendor/PERSONAL/template/header-footer/header.php");
+	require_once("vendor/PERSONAL/template/client-site/header-footer/header.php");
 	require_once("vendor/PERSONAL/template/client-site/lista-produtos.php");
-	require_once("vendor/PERSONAL/template/header-footer/footer.php");
+	require_once("vendor/PERSONAL/template/client-site/header-footer/footer.php");
 
 });
 
 $app->get('/carrinho', function() {
     
-	require_once("vendor/PERSONAL/template/header-footer/header.php");
+	require_once("vendor/PERSONAL/template/client-site/header-footer/header.php");
 	require_once("vendor/PERSONAL/template/client-site/carrinho.php");
-	require_once("vendor/PERSONAL/template/header-footer/footer.php");
+	require_once("vendor/PERSONAL/template/client-site/header-footer/footer.php");
 
 });
 
 $app->get('/esqueci-a-senha', function() {
     
-	require_once("vendor/PERSONAL/template/header-footer/header.php");
+	require_once("vendor/PERSONAL/template/client-site/header-footer/header.php");
 	require_once("vendor/PERSONAL/template/client-site/r-senha.php");
-	require_once("vendor/PERSONAL/template/header-footer/footer.php");
+	require_once("vendor/PERSONAL/template/client-site/header-footer/footer.php");
 
 });
 
 $app->get('/login', function() {
     
-	require_once("vendor/PERSONAL/template/header-footer/header.php");
+	require_once("vendor/PERSONAL/template/client-site/header-footer/header.php");
 	require_once("vendor/PERSONAL/template/client-site/login.php");
-	require_once("vendor/PERSONAL/template/header-footer/footer.php");
+	require_once("vendor/PERSONAL/template/client-site/header-footer/footer.php.php");
 
 });
 
 $app->get('/pagamento', function() {
     
-	require_once("vendor/PERSONAL/template/header-footer/header.php");
+	require_once("vendor/PERSONAL/template/client-site/header-footer/header.php");
 	require_once("vendor/PERSONAL/template/client-site/pagamento.php");
-	require_once("vendor/PERSONAL/template/header-footer/footer.php");
+	require_once("vendor/PERSONAL/template/client-site/header-footer/footer.php");
 
 });
 
 #ADMIN ROTES
+$app->get('/admin', function() {
+    
+	require_once("vendor/PERSONAL/template/adm-site/header-footer/header.php");
+	require_once("vendor/PERSONAL/template/adm-site/index.php");
+	require_once("vendor/PERSONAL/template/adm-site/header-footer/footer.php");
+
+});
 
 
 $app->run();
